366b52515433ebea4cb5cf549bb2cad4ec201497
Davlatbek <117518580+Davlatbek0710@users.noreply.github.com>
2023-01-21 13:23:56 +0500

    Update game_menu.py

--- keyboards/inline/game_menu.py
+++ keyboards/inline/game_menu.py
@@ -3,15 +3,15 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 all_games = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton('Dice🎲', callback_data='dice'),
+            InlineKeyboardButton('🎲Dice', callback_data='dice'),
             InlineKeyboardButton('Soccer⚽️', callback_data='soccer')
         ],
         [
-            InlineKeyboardButton('Bowling🎳', callback_data='bowling'),
+            InlineKeyboardButton('🎳Bowling', callback_data='bowling'),
             InlineKeyboardButton('Basketball🏀', callback_data='basketball')
         ],
         [
-            InlineKeyboardButton('Slot_Machine🎰', callback_data='slot_machine'),
+            InlineKeyboardButton('🎰Slot_Machine', callback_data='slot_machine'),
             InlineKeyboardButton('Dart🎯', callback_data='dart')
         ]
     ]
